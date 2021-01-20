7aa8b12e2ee1f3953570eb28918259e1a3bdf0ec
Belinda Davidson <belzuk@yahoo.com>
2021-01-20 11:58:47 -0600

    added filter buttons and worked on accessibility features

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import { nanoid } from "nanoid";
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 import Shop from "./components/Shop";
 
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: item => !item.completed,
+  Completed: item => item.completed
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP)
+
 function App(props) {
+  const [filter, setFilter] = useState('All');
   const [items, setItems] = useState(props.items);
   function addItem(name) {
     const newItem = { id: "shop-" + nanoid(), name: name, completed: false };
@@ -37,30 +54,45 @@ function App(props) {
     setItems(editedItemList);
   }
 
-  const itemList = items.map(item => (
-    <Shop 
-      id={item.id} 
-      name={item.name} 
+  const itemList = items.filter(FILTER_MAP[filter]).map(item => (
+    <Shop
+      id={item.id}
+      name={item.name}
       completed={item.completed}
       key={item.id}
-      toggleItemCompleted={toggleItemCompleted} 
+      toggleItemCompleted={toggleItemCompleted}
       deleteItem={deleteItem}
       editItem={editItem}
     />
   ));
   
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+
   const itemsNoun = itemList.length !== 1 ? 'items' : 'item';
   const headingText = `${itemList.length} ${itemsNoun} remaining`;
+  const listHeadingRef = useRef(null);
+  const prevItemLength = usePrevious(items.length);
+
+  useEffect(() => {
+    if (items.length - prevItemLength === -1) {
+      listHeadingRef.current.focus();
+    }
+  }, [items.length, prevItemLength]);
 
   return (
     <div className="shopapp stack-large">
       <Form addItem={addItem} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+      {filterList}
       </div>
-      <h2 id="list-heading">{headingText}</h2>
+      <h2 id="list-heading" tabIndex="-1" ref={listHeadingRef}>{headingText}</h2>
       <ul
         role="list"
         className="shop-list stack-large stack-exception"
--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+
+function usePrevious(value) {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
 
 export default function Shop(props) {
 
   const [isEditing, setEditing] = useState(false);
   const [newName, setNewName] = useState('');
+  const editFieldRef = useRef(null);
+  const editButtonRef = useRef(null);
+  const wasEditing = usePrevious(isEditing);
 
   function handleChange(e) {
     setNewName(e.target.value);
@@ -22,17 +33,19 @@ export default function Shop(props) {
         <label className="shop-label" htmlFor={props.id}>
           New name for {props.name}
         </label>
-        <input id={props.id} 
-          className="shop-text" 
-          type="text" 
-          value={newName} 
+        <input
+          id={props.id}
+          className="shop-text"
+          type="text"
+          value={newName}
           onChange={handleChange}
+          ref={editFieldRef}
         />
       </div>
       <div className="btn-group">
       <button
         type="button"
-        className="btn todo-cancel"
+        className="btn shop-cancel"
         onClick={() => setEditing(false)}
         >Cancel<span className="visually-hidden">renaming {props.name}</span>
       </button>
@@ -46,30 +59,42 @@ export default function Shop(props) {
   const viewTemplate = (
     <div className="stack-small">
       <div className="c-cb">
-          <input
-            id={props.id}
-            type="checkbox"
-            defaultChecked={props.completed}
-            onChange={() => props.toggleItemCompleted(props.id)}
-          />
-          <label className="shop-label" htmlFor={props.id}>
-            {props.name}
-          </label>
-        </div>
-        <div className="btn-group">
-          <button type="button" className="btn" onClick={() => setEditing(true)}>
-          Edit <span className="visually-hidden">{props.name}</span>
-          </button>
-          <button
-            type="button"
-            className="btn btn__danger"
-            onClick={() => props.deleteItem(props.id)}
-          >
-            Delete <span className="visually-hidden">{props.name}</span>
-          </button>
-        </div>
+        <input
+          id={props.id}
+          type="checkbox"
+          defaultChecked={props.completed}
+          onChange={() => props.toggleItemCompleted(props.id)}
+        />
+        <label className="shop-label" htmlFor={props.id}>
+          {props.name}
+        </label>
+      </div>
+      <div className="btn-group">
+        <button
+          type="button"
+          className="btn"
+          onClick={() => setEditing(true)}
+          ref={editButtonRef}
+          >Edit <span className="visually-hidden">{props.name}</span>
+        </button>
+        <button
+          type="button"
+          className="btn btn__danger"
+          onClick={() => props.deleteItem(props.id)}
+          >Delete <span className="visually-hidden">{props.name}</span>
+        </button>
+      </div>
     </div>
   );
+
+  useEffect(() => {
+    if (!wasEditing && isEditing) {
+      editFieldRef.current.focus();
+    }
+    if (wasEditing && !isEditing) {
+      editButtonRef.current.focus();
+    }
+  }, [wasEditing, isEditing]);
 
   return <li className="shop">{isEditing ? editingTemplate : viewTemplate}</li>;
 
